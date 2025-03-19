61f4aa5ef088090c56556cfcc3a1bd8ebcc7fd86
Nicholas Tam <nta59@sfu.ca>
2025-03-18 23:56:46 -0700

    feat(initialize_db) added several of the create statements to initalize_db.

--- main.py
+++ main.py
@@ -1,1 +1,90 @@
+import sqlite3
+
+
+
+
+def initialize_db(conn: sqlite3.Connection):
+    '''
+    - takes in a connection and creates all tables
+    - should read in data from a csv file
+    '''
+    #TODO: implement csv reading capabillity
+
+    #instantiate tables
+    create_Item = '''
+    CREATE TABLE Item (
+	    itemID INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL CHECK (LENGTH(title) <= 255),
+        authorFirstName TEXT CHECK (LENGTH(authorFirstName) <= 50), 
+        authorLastName TEXT CHECK (LENGTH(authorLastName) <= 100), 
+        format TEXT CHECK (format IN ('Book', 'eBook', 'CD', 'scientific journal', 'record', 'DVD', 'Blu ray', 'magazine')),
+        isAdded INTEGER CHECK (isAdded IN (0, 1))
+    );
+    '''
+
+    create_Fiction = '''
+    CREATE TABLE Fiction (
+    	itemID INTEGER PRIMARY KEY, 
+    	cutter CHAR(3) CHECK (LENGTH(cutter) = 3),
+    	FOREIGN KEY(itemID) REFERENCES Item(itemID) ON DELETE CASCADE
+    );
+    '''
+
+    create_NonFiction = '''
+    CREATE TABLE NonFiction (
+	    itemID INTEGER PRIMARY KEY,
+	    callNum REAL CHECK (callNum < 1000 AND callNum * 10000 = CAST(callNum * 10000 AS INTEGER)), 
+	    cutter CHAR(3) CHECK (LENGTH(cutter) = 3),
+	    FOREIGN KEY(itemID) REFERENCES Item(itemID) ON DELETE CASCADE
+    );
+    '''
+
+    create_patron = ''' 
+    CREATE TABLE Patron (
+    	patronID INTEGER PRIMARY KEY AUTOINCREMENT,
+    	firstName TEXT CHECK (LENGTH(firstName) <= 50), 
+    	lastName TEXT CHECK (LENGTH(lastName) <= 100)
+    ); 
+    '''
+
+    create_Employee = '''
+    CREATE TABLE Employee (
+    	employeeID INTEGER PRIMARY KEY AUTOINCREMENT,
+    	firstName TEXT CHECK (LENGTH(firstName) <= 50), 
+    	lastName TEXT CHECK (LENGTH(lastName) <= 100), 
+    	position TEXT, 
+    	salary INTEGER
+    ); 
+    '''
+
+
+
+#TODO: create DB functions
+'''
+Find an item in the library
+Borrow an item from the library
+Return a borrowed item
+Donate an item to the library
+Find an event in the library
+Register for an event in the library
+Volunteer for the library
+Ask for help from a librarian
+'''
+
+
+
+
+
+conn = sqlite3.connect("library.db")
+
+cursor = conn.cursor()
+
+
+
+
+
+
+with cursor:
+
+
 
